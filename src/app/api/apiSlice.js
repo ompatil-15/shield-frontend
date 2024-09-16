a9ac0352011a0060385ac1bf1055f6f17f79ac45
Om Patil <patilom001@gmail.com>
2024-09-16 19:00:29 +0530

    update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://ompatil-shield.onrender.com',
+    baseUrl: 'https://ompatil-shield-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
